36427ea7c153439728648ffd95cc565ea1eb4777
floz <floz_pub@hotmail.com>
2019-05-12 15:43:36 +0800

    update inital sql

--- init.sql
+++ init.sql
@@ -1,32 +1,39 @@
 /*==============================================================*/
-/* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2019/5/4 17:10:16                            */
+/* DBMS name:      MySQL 8.0                                    */
+/* Created on:     2019/5/12 15:32:38                           */
 /*==============================================================*/
 
 
 drop table if exists `card effect state`;
 
-drop table if exists `player cards`;
-
-drop table if exists `players desk`;
+drop table if exists `card`;
 
 drop table if exists `desk detail`;
-
-drop table if exists `match`;
-
-drop table if exists `player`;
-
-drop table if exists `card`;
 
 drop table if exists `desk`;
 
 drop table if exists `effect`;
 
+drop table if exists `match`;
 
+drop table if exists `player card`;
 
+drop table if exists `player`;
+
+drop table if exists `player desk`;
 
 /*==============================================================*/
-/* Table: card                                                  */
+/* Table: `card effect state`                                   */
+/*==============================================================*/
+create table `card effect state`
+(
+   `card id`            int not null,
+   `effect id`          int not null,
+   primary key (`card id`, `effect id`)
+);
+
+/*==============================================================*/
+/* Table: card                                                 */
 /*==============================================================*/
 create table `card`
 (
@@ -39,54 +46,46 @@ create table `card`
 );
 
 /*==============================================================*/
-/* Table: "card effect state"                                   */
-/*==============================================================*/
-create table `card effect state`
-(
-   `card id`            int not null,
-   `effect id`          int not null,
-   primary key (`card id`, `effect id`)
-);
-
-/*==============================================================*/
-/* Table: desk                                                  */
-/*==============================================================*/
-create table `desk`
-(
-   `class`              char(100),
-   `desk id`            int not null AUTO_INCREMENT,
-   `name`				char(100),
-   primary key (`desk id`)
-);
-
-/*==============================================================*/
-/* Table: "desk detail"                                         */
+/* Table: `desk detail`                                         */
 /*==============================================================*/
 create table `desk detail`
 (
    `card id`            int not null,
    `desk id`            int not null,
-   primary key (`card id`, `desk id`)
+   `card detail id`     int not null AUTO_INCREMENT,
+   primary key (`card detail id`)
 );
 
 /*==============================================================*/
-/* Table: effect                                                */
+/* Table: desk                                                 */
 /*==============================================================*/
-create table `effect`
+create table `desk`
+(
+   `class`                char(100),
+   `desk id`            int not null auto_increment,
+   `build type`         char(100),
+   primary key (`desk id`)
+);
+
+/*==============================================================*/
+/* Table: effects                                               */
+/*==============================================================*/
+create table `effects`
 (
    `description`          char(100),
-   `effect id`          int not null AUTO_INCREMENT,
+   `effect id`          int not null auto_increment,
    `effect type`        char(100),
    primary key (`effect id`)
 );
 
 /*==============================================================*/
-/* Table: "match"                                               */
+/* Table: matches                                               */
 /*==============================================================*/
-create table `match`
-( 
-   `round`              int,
-   `match id`           int not null AUTO_INCREMENT,
+create table `matches`
+(
+   `round`                int,
+   `process`              blob,
+   `match id`           int not null auto_increment,
    `winner id`          int not null,
    `loser id`           int not null,
    `loser desk id`      int not null,
@@ -95,70 +94,72 @@ create table `match`
 );
 
 /*==============================================================*/
-/* Table: player                                                */
+/* Table: `player card`                                        */
+/*==============================================================*/
+create table `player card`
+(
+   `card id`            int not null,
+   `player id`          int not null,
+   `player card id`    int not null auto_increment,
+   primary key (`player card id`)
+);
+
+/*==============================================================*/
+/* Table: player                                               */
 /*==============================================================*/
 create table player
 (
-   `money`              int,
-   `dust`               int,
-   `player id`          int not null AUTO_INCREMENT,
+   money                int,
+   dust                 int,
+   `player id`          int not null auto_increment,
    `player name`        char(100),
    primary key (`player id`)
 );
 
 /*==============================================================*/
-/* Table: "player cards"                                        */
+/* Table: `player desk`                                        */
 /*==============================================================*/
-create table `player cards`
-(
-   `card id`            int not null,
-   `player id`          int not null,
-   primary key (`card id`, `player id`)
-);
-
-/*==============================================================*/
-/* Table: "players desk"                                        */
-/*==============================================================*/
-create table `players desk`
+create table `player desk`
 (
    `player id`          int not null,
    `desk id`            int not null,
-   primary key (`player id`, `desk id`)
+   `player desk id`    int not null auto_increment,
+   primary key (`player desk id`)
 );
 
-alter table `card effect state` add constraint `effect` foreign key (`effect id`)
-      references effect (`effect id`) on delete restrict on update restrict;
+alter table `card effect state` add constraint effot foreign key (`effect id`)
+      references effects (`effect id`) on delete restrict on update restrict;
 
-alter table `card effect state` add constraint `effect card` foreign key (`card id`)
+alter table `card effect state` add constraint card foreign key (`card id`)
       references card (`card id`) on delete restrict on update restrict;
 
-alter table `desk detail` add constraint `detail desk` foreign key (`desk id`)
+alter table `desk detail` add constraint desk foreign key (`desk id`)
       references desk (`desk id`) on delete restrict on update restrict;
 
-alter table `desk detail` add constraint `desk card` foreign key (`card id`)
+alter table `desk detail` add constraint card foreign key (`card id`)
       references card (`card id`) on delete restrict on update restrict;
 
-alter table `match` add constraint `loser` foreign key (`loser id`)
+alter table matches add constraint loser foreign key (`loser id`)
       references player (`player id`) on delete restrict on update restrict;
 
-alter table `match` add constraint `loser_desk` foreign key (`loser desk id`)
+alter table matches add constraint loser_desk foreign key (`loser desk id`)
       references desk (`desk id`) on delete restrict on update restrict;
 
-alter table `match` add constraint `winner` foreign key (`winner id`)
+alter table matches add constraint winner foreign key (`winner id`)
       references player (`player id`) on delete restrict on update restrict;
 
-alter table `match` add constraint `winner_desk` foreign key (`winner desk id`)
+alter table matches add constraint winner_desk foreign key (`winner desk id`)
       references desk (`desk id`) on delete restrict on update restrict;
 
-alter table `player cards` add constraint `FK_card2` foreign key (`player id`)
+alter table `player card` add constraint FK_card2 foreign key (`player id`)
       references player (`player id`) on delete restrict on update restrict;
 
-alter table `player cards` add constraint `FK_player2` foreign key (`card id`)
+alter table `player card` add constraint FK_player2 foreign key (`card id`)
       references card (`card id`) on delete restrict on update restrict;
 
-alter table `players desk` add constraint `player` foreign key (`player id`)
+alter table `player desk` add constraint player foreign key (`player id`)
       references player (`player id`) on delete restrict on update restrict;
 
-alter table `players desk` add constraint `player desk` foreign key (`desk id`)
+alter table `player desk` add constraint desk foreign key (`desk id`)
       references desk (`desk id`) on delete restrict on update restrict;
 
